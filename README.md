f65c985e0c02d21cb0811408195c1cf1e20050ce
JokaZhao <zhaozengjie@zhongan.com>
2019-09-20 19:31:18 +0800

    补充建表语句和日志

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 - MyBatis
 - IBatis
 - 多表查询支持
+
+## DB DDL脚本
+    CREATE TABLE `user` (
+      `id` bigint(20) NOT NULL AUTO_INCREMENT,
+      `user_name` varchar(45) NOT NULL COMMENT '用户姓名',
+      `user_no` varchar(20) NOT NULL COMMENT '用户标识码',
+      `user_type` tinyint(2) NOT NULL COMMENT '用户标识类型:1-身份证号,2-手机号码',
+      `status` tinyint(2) NOT NULL DEFAULT '1' COMMENT '状态:1-生效,0-无效',
+      `is_deleted` char(1) NOT NULL DEFAULT 'N' COMMENT '是否删除',
+      `gmt_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+      `creator` varchar(64) NOT NULL DEFAULT 'system' COMMENT '创建人ID',
+      `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+      `modifier` varchar(64) NOT NULL DEFAULT 'system' COMMENT '修改人ID',
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `uniq_user_name_user_no` (`user_name`,`user_no`) USING BTREE,
+      KEY `idx_user_no` (`user_no`) USING BTREE
+    ) ENGINE=InnoDB AUTO_INCREMENT=10019 DEFAULT CHARSET=utf8 COMMENT='用户信息表'
+   
+## 初始化数据 DML脚本
+    INSERT INTO test.`user`(id, user_name, user_no, user_type, status, is_deleted, gmt_created, creator, gmt_modified, modifier)
+    VALUES(10000, '张三', '18779880000', 1, 1, 'N', '2019-09-17 19:30:59.000', 'system', '2019-09-17 19:30:59.000', 'system');
--- mybatis/src/main/java/com/joka/batisdemo/mybatis/MyBatisStater.java
+++ mybatis/src/main/java/com/joka/batisdemo/mybatis/MyBatisStater.java
@@ -1,15 +1,25 @@
 package com.joka.batisdemo.mybatis;
 
+import com.alibaba.fastjson.JSONObject;
 import com.joka.batisdemo.common.dto.UserDTO;
+import com.joka.batisdemo.common.util.IDCardNoUtils;
+import com.joka.batisdemo.common.util.NameUtils;
 import com.joka.batisdemo.mybatis.dao.UserDao;
+import com.sun.javafx.binding.StringFormatter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created on 2019/9/17 19:16.
@@ -19,20 +29,87 @@ import java.util.List;
  */
 public class MyBatisStater {
 
+    private static Logger logger = LoggerFactory.getLogger(MyBatisStater.class);
+
+    static SqlSessionFactory sqlSessionFactory;
+
+    static Optional<SqlSession> sqlSession;
+
     public static void main(String[] args) throws IOException {
 
+        try {
+            init();
+
+//            selectAll();
+//
+//            selectOne();
+//
+//            selectByParam();
+            insertOne();
+
+        } finally {
+            sqlSession.ifPresent(SqlSession::close);
+        }
+    }
+
+    public static void init() throws IOException {
         String resource = "datasource/mybatis/mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSession = Optional.of(sqlSessionFactory.openSession());
+    }
 
-        try (SqlSession sqlSession = sqlSessionFactory.openSession()){
-            final UserDao mapper = sqlSession.getMapper(UserDao.class);
-            final List<UserDTO> userDTOS = mapper.selectAll();
+    public static void selectAll(){
+        sqlSession.ifPresent(r -> {
+            final UserDao mapper = r.getMapper(UserDao.class);
+            List<UserDTO> userDTOS = mapper.selectAll();
+            System.out.println(StringFormatter.format("selectAll : %d",JSONObject.toJSONString(userDTOS)));
 
+        });
+    }
 
-            System.out.println(userDTOS.toString());
-        }
+    public static void selectOne(){
+        sqlSession.ifPresent(r -> {
+            UserDao mapper = r.getMapper(UserDao.class);
+            UserDTO userDTO = mapper.selectById(10000L);
+            System.out.println(StringFormatter.format("selectOne : %d",JSONObject.toJSONString(userDTO)));
+        });
+    }
+
+    public static void selectByParam(){
+        sqlSession.ifPresent(r -> {
+            UserDao mapper = r.getMapper(UserDao.class);
+            Map<String,Object> param = new HashMap<>();
+            param.put("user_name","张三");
+            List<UserDTO> userDTOS1 = mapper.selectByParam(param);
+            System.out.println(StringFormatter.format("selectByParam : %d",JSONObject.toJSONString(userDTOS1)));
+        });
+    }
+
+    public static void selectIn(){
+
 
 
     }
+
+    public static void insertOne(){
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserName(NameUtils.getName());
+        userDTO.setUserNo(IDCardNoUtils.getRandomID());
+        userDTO.setUserType("1");
+        userDTO.setStatus("1");
+
+        sqlSession.ifPresent(r -> {
+            UserDao mapper = r.getMapper(UserDao.class);
+            try {
+                mapper.insertOne(userDTO);
+
+            }catch (Exception e){
+                logger.error("",e);
+            }
+        });
+
+    }
+
 }
